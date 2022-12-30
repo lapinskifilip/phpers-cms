59cf4380b75aba6107133099ec3d80f0da7b90a0
Filip Łapiński <lapinski.filipp@gmail.com>
2022-12-30 12:09:23 +0100

    clear the repo

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-const SITE_NAME = 'phpers.org';
-const SITE_TITLE = 'phpers.org';
-
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
-const DB_NAME = 'phpers';
