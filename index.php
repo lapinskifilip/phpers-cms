8f695d4ad15b2cb2cea91c70e593f5d74b3f67ef
Filip Łapiński <lapinski.filipp@gmail.com>
2023-01-05 13:45:35 +0100

    testing commit

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-class HappyNewYear
-{
-    public function __construct(
-        protected string $firstName,
-        protected string $lastName
-    ) {
-        return $this->$firstName . ' ' . $this->$lastName;
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hello, choose what do you want to do below.</h1>
+    <hr>
+    <a href="../login.php">Login</a>
+    <a href="../register.php">Register</a>
+
+    <h2>Users list</h2>
+
+    <?php
+
+    use App\Account\User;
+
+    include_once "./Config/db.php";
+
+    $user = new User($pdo);
+    $list = $user->getUsers();
+
+    foreach ($list as $test) {
+        echo $test["login"], "\n";
     }
-}
+    ?>
+</body>
+
+</html>
